1a780d9dcd2166dc08e22bb39c201a609147c175
Igor Kostiuchenko <i.kostiuchenko@twinwingames.com>
2022-11-15 18:03:30 +0200

    background added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application } from 'pixi.js'
+import { Application, Texture, TilingSprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("canvas") as HTMLCanvasElement,
@@ -11,3 +11,14 @@ const app = new Application({
 });
 
 app.renderer.view.style.position = 'absolute';
+
+const cloudsTexture: Texture = Texture.from("clouds.png");
+const cloudsSprite: TilingSprite = new TilingSprite(
+  cloudsTexture,
+  app.screen.width,
+  app.screen.height
+)
+cloudsSprite.tileScale.set(0.5, 0.5);
+app.ticker.add(() => cloudsSprite.tilePosition.x += 1);
+
+app.stage.addChild(cloudsSprite);
